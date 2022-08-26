30e15c7b8b5773d5d7c45a793354d77a1e1b29da
William Chalk <3halk1@gmail.com>
2022-08-25 20:35:54 -0400

    added form and dropdown submission

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,13 @@
-var buttonE1 = document.getElementById("save-task");
+var formEl = document.querySelector("#task-form");
 
 var tasksToDoE1 = document.getElementById("tasks-to-do");
 
-var createTaskHandler = function(){
-    var listItemE1 = document.createElement("li");
-    listItemE1.className = "task-item";
-    listItemE1.textContent = "This is a new task.";
-    tasksToDoE1.appendChild(listItemE1);
+var createTaskHandler = function (event) {
+  event.preventDefault();
+  var listItemE1 = document.createElement("li");
+  listItemE1.className = "task-item";
+  listItemE1.textContent = "This is a new task.";
+  tasksToDoE1.appendChild(listItemE1);
 };
 
-buttonE1.addEventListener("click",createTaskHandler());
+formEl.addEventListener("submit", createTaskHandler);
